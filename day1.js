8952b48ccf8aaead850df95abbe7ba68664b8e9a
Tomáš Oktábec <t.oktabec@bkbn.com>
2022-12-02 14:06:23 +0900

    Day 1 - part 2

--- day1.js
+++ day1.js
@@ -52,4 +52,43 @@ function getHighestCalories(input) {
     return maxCalories
 }
 
-console.log(getHighestCalories(inputString))
+console.log(`Top elf calories: ${getHighestCalories(inputString)}\n\n`)
+
+/*
+    --- Part Two ---
+    By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+
+    To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
+
+    In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
+
+    Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+*/
+
+function getTop3HighestCalories(input) {
+    let top3 = [0, 0, 0]
+
+  
+    for (let elf of input.split('\n\n')) {
+        const elfTotal = elf.split('\n').reduce((total, currentItem) => total + Number(currentItem), 0)
+
+        switch (true) {
+            case elfTotal > top3[0]:
+                top3.unshift(elfTotal)
+                break
+            
+            case elfTotal > top3[1]:
+                top3.splice(1, 0, elfTotal)
+                break
+
+            case elfTotal > top3[2]:
+                top3.splice(2, 1, elfTotal)
+                break
+        }
+
+        top3.length = 3
+    }
+    return top3[0] + top3[1] + top3[2]
+}
+
+console.log(`Top 3 elves calories: ${getTop3HighestCalories(inputString)}`)
