a05cd1070b043e384cba8ac0019e2c25a125feea
Tomáš Oktábec <t.oktabec@bkbn.com>
2022-12-02 15:11:12 +0900

    Day 2 - part 2

--- day2.js
+++ day2.js
@@ -29,7 +29,6 @@
 
 const strategySheet = ``
 
-
 const combinationResults = {
     P: {
         R: 'WIN',
@@ -82,11 +81,11 @@ function getShapes(stringInput) {
     return stringInput.split(' ').map((sign) => codeMap[sign])
 }
 
-function getGameResult(strategy) {
+function getGameResult(strategy, shapeResolver) {
     const rounds = strategy.split('\n')
 
     const score = rounds.reduce((total, round) => {
-        const shapes = getShapes(round)
+        const shapes = shapeResolver(round)
         
         console.log(`Playing round with combination: ${round} => ${shapes.join(' ')}\n`)
 
@@ -96,7 +95,50 @@ function getGameResult(strategy) {
         return total + result.score
     }, 0)
 
-    console.log(`TOTAL SCORE: ${score}`)
+    return score
 }
 
-getGameResult(strategySheet)
+console.log(`TOTAL SCORE WITH MISUNDERSTOOD STRATEGY: ${getGameResult(strategySheet, getShapes)}\n\n-----------------------------------\n\n`)
+
+
+/*
+    --- Part Two ---
+    The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+    The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+    In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+    In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+    In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+    Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+    Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+*/
+
+const responseMap = {
+    P: {
+        'X': 'R',
+        'Y': 'P',
+        'Z': 'S',
+    },
+    R: {
+        'Y': 'R',
+        'Z': 'P',
+        'X': 'S',
+    },
+    S: {
+        'Z': 'R',
+        'X': 'P',
+        'Y': 'S',
+    },
+}
+
+
+function getCorrectShapes(stringInput) {
+    const inputArray = stringInput.split(' ')
+    const opponentShape = codeMap[inputArray[0]]
+    const correctResponse = responseMap[opponentShape][inputArray[1]]
+    return [opponentShape, correctResponse]
+}
+
+console.log(`TOTAL SCORE WITH CORRECTED STRATEGY: ${getGameResult(strategySheet, getCorrectShapes)}`)
