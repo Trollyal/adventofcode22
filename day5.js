84bc269559a322fac025457a80dc42490b513e85
Tomáš Oktábec <t.oktabec@bkbn.com>
2022-12-05 18:34:33 +0900

    Day 5 - part 2

--- day5.js
+++ day5.js
@@ -594,27 +594,78 @@ function getInitialState(input) {
     }
 }
 
-function executeInstruction(instruction, deck) {
+function executeInstruction(instruction, deck, shouldReverse = true) {
     const [, count, , sourceColumn, , destinationColumn] = instruction.split(' ')
 
     console.log("executing: ", instruction)
     
-    const payload = deck[+sourceColumn].splice(0, +count).reverse()
+    const payload = deck[+sourceColumn].splice(0, +count)
+    if (shouldReverse) payload.reverse()
+    
     deck[+destinationColumn].unshift(...payload)
 
     console.log("==> ", deck)
 }
 
-function getFinalDeckState(input) {
+function getFinalDeckState(input, canMoveMultiple) {
     const {deck, instructions} = getInitialState(input)
 
     console.log("INIT DECK:", deck)
 
     for (let instruction of instructions) {
-        executeInstruction(instruction, deck)
+        executeInstruction(instruction, deck, !canMoveMultiple)
     }
 
     return deck.map((stack) => stack[0]).join('')
 }
 
-console.log(getFinalDeckState(input))
+console.log(getFinalDeckState(input, false))
+
+
+/*
+    --- Part Two ---
+    As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+    Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+    The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+    Again considering the example above, the crates begin in the same configuration:
+
+        [D]    
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
+    Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+    [D]        
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
+    However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+            [D]
+            [N]
+        [C] [Z]
+        [M] [P]
+    1   2   3
+    Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+            [D]
+            [N]
+    [C]     [Z]
+    [M]     [P]
+    1   2   3
+    Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+            [D]
+            [N]
+            [Z]
+    [M] [C] [P]
+    1   2   3
+    In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+    Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+*/
+
+console.log(getFinalDeckState(input, true))
